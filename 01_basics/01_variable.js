caee66d85edf56478565d7237ba01b60e387a4c4
Bhavya Singhal <163103865+Bhavya56s@users.noreply.github.com>
2024-03-12 13:11:40 +0000

    variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountID = 145333
+let accountEmail= " dhruvsingal2005@gmail.com"
+var accountPassword = "1234"
+accountCity = "Jaipur"
+
+//accountID=6//not allowed
+ accountEmail= " dhrngal2005@gmail.com"
+  accountPassword = "1238884"
+accountCity = "udaiur"
+
+console.log(accountID);
+
+console.table([accountID,accountEmail,accountPassword,accountCity])
